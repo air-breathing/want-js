e5a2d75e8d815c1645409e7002c670b1d7fbeaca
Nadezhda Grin <nadya-grin@yandex-team.ru>
2018-03-22 15:21:12 +0500

    Правка в локальном конфиге

--- .want-js.config.js
+++ .want-js.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     commandParams: {
         'pull-request': {
+            summary: 'Open pull request on github.com',
+
             githubApiUrl: 'github.com',
             githubApiPath: '',
             githubApiOauthTokenName: 'COMMON_GITHUB_OAUTH_TOKEN'
